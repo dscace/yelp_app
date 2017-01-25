344cccd8fa00f0eba14b2ab4db32c5a4ca2a035d
dscace <derek.scace@gmail.com>
2017-01-25 14:03:02 -0700

    Change font colors and well heights

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   post 'results' => 'home#results'
 
-  get 'test' => 'home#test'
+  get 'results' => 'home#results'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
