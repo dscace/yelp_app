a47731ca64fa1e5e0f5ef8aef25411ff8d06ef31
dscace <derek.scace@gmail.com>
2017-01-26 19:56:17 -0700

    Precompile Font ttf

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module YelpApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
   end
 end
